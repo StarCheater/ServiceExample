36af4ac26fd344d49f021e1832ea039387c4489c
starcheater <starcheater@gmail.com>
2020-08-27 13:03:58 +0300

    Добавьте файлы проекта.

---
+++ ServiceMain.cs
@@ -0,0 +1,21 @@
+﻿using System.ServiceProcess;
+
+namespace ServiceExample
+{
+    public partial class ServiceMain : ServiceBase
+    {
+        public ServiceMain()
+        {
+            InitializeComponent();
+            this.CanStop = true;
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
